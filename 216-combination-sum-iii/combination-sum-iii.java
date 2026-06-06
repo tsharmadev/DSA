ab411aa19ef37ea7df3237f9c8e4fac436bb3873
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-07 04:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (46.69%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,39 +1,29 @@
-
 class Solution {
-    private void func(int sum, int last, List<Integer> nums, int k, List<List<Integer>> ans) {
-        // If the sum is zero and the number of elements is k
-        if (sum == 0 && nums.size() == k) {
-            // Add the current combination to the answer
-            ans.add(new ArrayList<>(nums));
-            return;
-        }
-        // If the sum is less than or equal to zero or the number of elements exceeds k
-        if (sum <= 0 || nums.size() > k) return;
-
-        // Iterate from the last number to 9
-        for (int i = last; i <= 9; i++) {
-            // If the current number is less than or equal to the sum
-            if (i <= sum) {
-                // Add the number to the current combination
-                nums.add(i);
-                // Recursive call with updated sum and next number
-                func(sum - i, i + 1, nums, k, ans);
-                // Remove the last number to backtrack
-                nums.remove(nums.size() - 1);
-            } else {
-                // If the number is greater than the sum, break the loop
-                break;
-            }
-        }
-    }
-
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        // Call the recursive function with initial parameters
-        func(n, 1, nums, k, ans);
+    public List<List<Integer>> combinationSum3(int k,int n) {
+        
+       
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>(); 
+        solve(1,k,n,0,ans,curr);
         return ans;
-    }
-}
 
-   
+    }
+   public void solve (int num,int k,int n,int sum,List<List<Integer>>ans, List<Integer>curr){
+    if((sum==n)&&(curr.size()==k)){
+        ans.add(new ArrayList<>(curr));
+        return;
+    }
+    if ((sum>n)||(curr.size()>k) ){
+        return;
+    }
+    if (num>9){
+        return;
+    }
+    curr.add(num);
+    solve(num+1,k,n,sum+num,ans,curr);
+    curr.remove(curr.size()-1);
+    solve(num+1,k,n,sum,ans,curr);
+
+   }
+    
+}

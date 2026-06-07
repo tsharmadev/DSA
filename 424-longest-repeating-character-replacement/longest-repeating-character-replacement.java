1972670022113ed8df15cc32e9685370da5fd5f4
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-07 22:48:27 +0530

    Time: 7 ms (89.06%) | Memory: 46 MB (78.05%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,18 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxLen = 0;
-        int maxFreq = 0;
-        int l = 0, r = 0;
+    int maxLen=0;
+        int maxFreq=0;
+        int l=0;
+        int r=0;
+        int n=s.length();
         int[] hash = new int[26];
-        while (r < s.length()) {
-            hash[s.charAt(r) - 'A']++;
-            maxFreq = Math.max(maxFreq, hash[s.charAt(r) - 'A']);
-            if ((r - l + 1) - maxFreq > k) {
-                hash[s.charAt(l) - 'A']--;
+        while(r<n){
+            hash[s.charAt(r)-'A']++;
+            maxFreq=Math.max(maxFreq,hash[s.charAt(r)-'A']);
+            while((r-l+1)-maxFreq>k){
+                hash[s.charAt(l)-'A']--;
                 l++;
             }
-            maxLen = Math.max(maxLen, r - l + 1);
+            maxLen=Math.max((r-l+1),maxLen);
             r++;
+
         }
         return maxLen;
     }

971dceca62e9b7e889bc53ba7150e5edc53d593c
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-13 00:49:51 +0530

    Time: 2 ms (83.65%) | Memory: 46.7 MB (28.35%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,65 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        
+        // Initialize board with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        backtrack(solutions, board, 0, n);
+        return solutions;
+    }
+    
+    private void backtrack(List<List<String>> solutions, char[][] board, int row, int n) {
+        if (row == n) {
+            // Found a valid solution
+            solutions.add(constructBoard(board));
+            return;
+        }
+        
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q'; // Place queen
+                backtrack(solutions, board, row + 1, n); // Recurse to next row
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+    
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        // Check column above
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private List<String> constructBoard(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            solution.add(new String(board[i]));
+        }
+        return solution;
+    }
+}

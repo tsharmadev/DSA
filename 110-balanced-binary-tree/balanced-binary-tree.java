c38e4002ca51fafe18054ab7d80907c6eb0a51a4
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-25 18:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (75.89%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int depth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return 1+ Math.max(depth(root.left),depth(root.right));
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        int leftHeight=depth(root.left);
+        int rightHeight=depth(root.right);
+        if(Math.abs(leftHeight-rightHeight)>1) return false;
+        return ((isBalanced(root.left))&&(isBalanced(root.right)));
+    }
+}

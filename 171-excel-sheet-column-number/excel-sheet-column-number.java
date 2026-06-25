685ac12e0102001fcd6147b987c157ac20b1d9f6
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-25 23:25:08 +0530

    Time: 1 ms (91.79%) | Memory: 43.5 MB (71.50%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -4,7 +4,7 @@ class Solution {
         
         for (int i = 0; i < columnTitle.length(); i++) {
             char c = columnTitle.charAt(i);
-            int value = c - 'A' + 1;  // Convert A=1, B=2, ..., Z=26
+            int value = c - 'A' + 1;  
             result = result * 26 + value;
         }
         

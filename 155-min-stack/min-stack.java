d79bda0570163e2768691826d792514b48c7c1be
Recursive Valkyrie <tanishaasha01@gmail.com>
2026-06-27 23:23:52 +0530

    Time: 35 ms (18.80%) | Memory: 99.9 MB (28.76%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,41 @@
+class MinStack {
+    Stack <Integer> stack;
+    Stack <Integer> minstack;
+    public MinStack() {
+        stack=new Stack<>();
+        minstack=new Stack<>();
+    }
+    
+    public void push(int val) {
+    stack.push(val);
+
+    if (minstack.isEmpty() || val <= minstack.peek()) {
+        minstack.push(val);
+    }
+}
+    
+    public void pop() {
+        if(stack.peek().equals(minstack.peek())){
+            minstack.pop();
+        }
+        stack.pop();
+
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

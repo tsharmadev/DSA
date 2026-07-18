41c43fe7c6a178d5ab1c61c97f1ba9e037c4bfd8
Tanisha Sharma <tanishaasha01@gmail.com>
2026-07-18 19:35:45 +0530

    Time: 1 ms (71.83%) | Memory: 43.6 MB (51.66%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,44 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
+
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         if (root == null) {
-            return result;
+            return ans;
         }
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
             for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == size - 1) {
-                    result.add(node.val);
+                TreeNode curr = q.poll();
+                if (level == ans.size()) {
+                    ans.add(curr.val);
                 }
-                if (node.left != null) {
-                    queue.add(node.left);
+                if (curr.right != null) {
+                    q.offer(curr.right);
                 }
-                if (node.right != null) {
-                    queue.add(node.right);
+                if (curr.left != null) {
+                    q.offer(curr.left);
                 }
             }
+            level++;
         }
-        return result;
+        return ans;
     }
 }
